2470deb7f0f19d83419807d6435ac21c95aa8bfa
andreicostandache <andrei97mihai@gmail.com>
2018-04-29 16:58:10 +0300

    Added components for the main window

--- Implementation/src/sample/Main.java
+++ Implementation/src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Brain Simulator");
+        primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
+  
     }
 
 
