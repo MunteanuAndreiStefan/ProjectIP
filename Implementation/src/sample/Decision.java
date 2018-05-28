63b9dbe145f909a7514650b9ebe0e690cf39e660
claudiu-ghiga <claudiu.ghiga@gmail.com>
2018-05-28 11:21:52 +0300

    Fixes #19

--- Implementation/src/sample/Decision.java
+++ Implementation/src/sample/Decision.java
@@ -16,7 +16,7 @@ public class Decision {
         Map<String, Integer> count = new HashMap<String, Integer>();
         for(int i=0; i<objects.size(); i++){
             String concept = (String)objects.get(i).get("found concept");
-            String percent = (String)objects.get(i).get("Concept matching");
+            String percent = objects.get(i).get("Concept matching").toString();
             //System.out.println(concept + " " + percent);
             if(count.containsKey(concept)) {
                 Double upd = Math.max(Double.parseDouble(percent), results.get(concept));
